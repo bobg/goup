159db4847739cac1258b71eb13ae5b681d26a7ad
Bob Glickstein <bobg@emphatic.com>
2025-03-17 15:28:04 -0700

    More initial development.

--- go.mod
+++ go.mod
@@ -5,5 +5,9 @@ go 1.24.0
 require (
 	github.com/bobg/errors v1.1.0
 	github.com/bobg/goproxyclient v0.1.0
+	github.com/bobg/mid v1.8.0
 	golang.org/x/mod v0.24.0
+	golang.org/x/time v0.11.0
 )
+
+require github.com/pkg/errors v0.9.1 // indirect
--- main.go
+++ main.go
@@ -4,14 +4,19 @@ package main
 import (
 	"context"
 	"debug/buildinfo"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/bobg/errors"
 	"github.com/bobg/goproxyclient"
+	"github.com/bobg/mid"
 	"golang.org/x/mod/semver"
+	"golang.org/x/time/rate"
 )
 
 func main() {
@@ -32,64 +37,101 @@ func run() error {
 	}
 
 	var (
-		client = goproxyclient.New(goproxy, nil)
-		ctx    = context.Background()
+		upgradeable bool
+		qps         float64
 	)
-	for _, arg := range os.Args[1:] {
+
+	flag.StringVar(&goproxy, "proxy", goproxy, "Go module proxy URL")
+	flag.BoolVar(&upgradeable, "u", false, "show only upgradeable files")
+	flag.Float64Var(&qps, "rate", 2, "max queries per second to the proxy")
+	flag.Parse()
+
+	var (
+		limiter = rate.NewLimiter(rate.Limit(qps), 1)
+		lt      = mid.LimitedTransport{L: limiter}
+		hc      = &http.Client{Transport: lt}
+		c       = controller{client: goproxyclient.New(goproxy, hc), upgradeable: upgradeable}
+		ctx     = context.Background()
+	)
+	for _, arg := range flag.Args() {
 		info, err := os.Stat(arg)
 		if err != nil {
 			return errors.Wrapf(err, "statting %s", arg)
 		}
 		if info.IsDir() {
-			if err := doDir(ctx, client, arg); err != nil {
+			if err := c.doDir(ctx, arg); err != nil {
 				return errors.Wrapf(err, "processing %s", arg)
 			}
 			continue
 		}
-		if err := doFile(ctx, client, arg); err != nil {
+		if err := c.doFile(ctx, arg); err != nil {
 			return errors.Wrapf(err, "processing %s", arg)
 		}
 	}
 	return nil
 }
 
-func doDir(ctx context.Context, client *goproxyclient.Client, dir string) error {
+type controller struct {
+	client      *goproxyclient.Client
+	upgradeable bool
+}
+
+func (c controller) doDir(ctx context.Context, dir string) error {
 	entries, err := os.ReadDir(dir)
 	if err != nil {
 		return errors.Wrapf(err, "reading %s", dir)
 	}
 	for _, entry := range entries {
-		if err := doFile(ctx, client, filepath.Join(dir, entry.Name())); err != nil {
+		if err := c.doFile(ctx, filepath.Join(dir, entry.Name())); err != nil {
 			return errors.Wrapf(err, "processing %s/%s", dir, entry.Name())
 		}
 	}
 	return nil
 }
 
-func doFile(ctx context.Context, client *goproxyclient.Client, file string) error {
+type output struct {
+	File        string `json:"file"`
+	Installed   string `json:"installed"`
+	Available   string `json:"available"`
+	MainModule  string `json:"main_module"`
+	MainPackage string `json:"main_package"`
+}
+
+func (c controller) doFile(ctx context.Context, file string) error {
 	info, err := buildinfo.ReadFile(file)
 	if err != nil {
 		return errors.Wrapf(err, "reading %s", file)
 	}
 
+	if c.upgradeable && !semver.IsValid(info.Main.Version) {
+		return nil
+	}
+
 	// xxx check info.GoVersion, is it out of date?
 
-	versions, err := client.List(ctx, info.Main.Path)
+	versions, err := c.client.List(ctx, info.Main.Path)
 	if err != nil {
 		return errors.Wrapf(err, "listing versions for %s", info.Main.Path)
 	}
 
-	if len(versions) == 0 {
-		fmt.Printf("%s: no versions found for %s\n", file, info.Main.Path)
+	o := output{
+		File:        file,
+		Installed:   info.Main.Version,
+		MainModule:  info.Main.Path,
+		MainPackage: info.Path,
+	}
+
+	if len(versions) > 0 {
+		semver.Sort(versions)
+		o.Available = versions[len(versions)-1]
+	}
+
+	if c.upgradeable && semver.Compare(o.Installed, o.Available) >= 0 {
 		return nil
 	}
 
-	semver.Sort(versions)
-	if semver.Compare(info.Main.Version, versions[len(versions)-1]) < 0 {
-		fmt.Printf("%s: at version %s, can be updated with go install %s@%s\n", file, info.Main.Version, info.Main.Path, versions[len(versions)-1])
-	} else {
-		fmt.Printf("%s: at version %s@%s, no update needed\n", file, info.Main.Path, info.Main.Version)
-	}
-
-	return nil
+	enc := json.NewEncoder(os.Stdout)
+	enc.SetIndent("", "  ")
+	err = enc.Encode(o)
+	return errors.Wrap(err, "encoding output")
 }
